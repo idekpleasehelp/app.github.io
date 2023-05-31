7a9f5db02b142a86b958d927f775a8cb77fb7c21
SWL <95669495+idekpleasehelp@users.noreply.github.com>
2023-06-01 01:33:48 +0530

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function playaudio(audioName) {
-    let audio = new Audio(audioName)
-    html5: true
-    audio.play()
-}
-playaudio("audio.wav")
+// function playaudio(audioName) {
+//     let audio = new Audio(audioName)
+//     html5: true
+//     audio.play()
+// }
+// playaudio("audio.wav")
